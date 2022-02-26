70f98648af34224d799f0865e0aef5e171d2dd6b
Shaxzod1712 <shukurullaev1712@gmail.com>
2022-02-26 19:24:55 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React  from "react";
 import './App.css'
-import Dars from './onState'
+import Input from "./Input";
 
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                <Dars/>
+                <Input />
             </div>
         )
     }

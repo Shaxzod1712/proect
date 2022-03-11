dcb320888091b5d094dd22431aee553b7b649cbb
Shaxzod1712 <shukurullaev1712@gmail.com>
2022-03-11 20:09:37 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React  from "react";
-import './App.css'
-import Input from "./Input";
+import React, {useState, useEffect}  from "react";
 
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Input />
-            </div>
-        )
+const App = () => {
+
+    const localData = localStorage.getItem('fruits')
+    const [name, setName] = useState('')
+    const [todos, setTodos] = useState(localData ? JSON.parse(localData) : [])
+
+    const onSabmit =()=>{
+        setTodos((prev)=>(
+            [...prev, {id: Date.now(), name: name}]
+        ))
+        setName('')
     }
+
+    useEffect(()=>{
+        localStorage.setItem('fruits', JSON.stringify(todos))
+    }, [todos])
+
+    const Clear =()=>{
+        localStorage.clear()
+        setTodos([])
+    }
+    return(
+        <div>
+            <input value={name} onChange={(e)=>setName(e.target.value)}/>
+            <button onClick={onSabmit}>Submit</button>
+            <div>
+                {
+                    todos.map((value)=>(
+                        <h1>{value.name}</h1>
+                    ))
+                }
+            </div>
+            <button onClick={Clear}>clear</button>
+        </div>
+    )
 }
 
 export default App
